9ec3a9a40020385d5cd2fb8d79f9cc126312321b
Chris Kay <chriskay25@gmail.com>
2019-04-14 02:34:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,4 @@
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 rooms = [1,2,3,4,5,6,7]
 
 def badge_maker(name)
