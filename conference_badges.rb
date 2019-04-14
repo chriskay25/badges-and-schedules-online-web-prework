b2e162b409fa8abefc9c24129cf5091fb75cd171
Chris Kay <chriskay25@gmail.com>
2019-04-14 02:05:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(array)
+  array.each do |names|
+    badge_maker(names)
